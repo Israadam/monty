13a870db618acc140f6e89a7d83a1c1f0c7a1360
htker <hhtkerrobert@gmail.com>
2023-12-13 21:22:37 +0200

    Stacks1

--- pint.c
+++ pint.c
@@ -5,15 +5,13 @@
  * @counter: line_number
  * Return: no return
 */
-void f_pint(stack_t **head, unsigned int counter)
-{
-	if (*head == NULL)
-	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	printf("%d\n", (*head)->n);
+void pint(int line_number) {
+  if (stack_size == 0) {
+    fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+    exit(EXIT_FAILURE);
+  }
+
+  int top_element = stack[stack_size - 1];
+  printf("%d\n", top_element);
 }
+
